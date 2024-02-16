8437e9e5d4a913ebf59800b2363150094da0c292
Ahlek <ahleksanael@gmail.com>
2024-02-16 15:35:30 +0100

    moar comments

--- src/shell.c
+++ src/shell.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002, Simon Nieupidiarts
+ * Copyright (C) 2002, Simon Nieuviarts
  */
 
 #include <stdio.h>
@@ -9,7 +9,7 @@
 #include <stdbool.h>
 
 
-int main(int argc, char**argpid, char **enpidp) {
+int main(int argc, char**argv, char **envp) {
 	while (1) {
 		int pipe_in = 0;
 		struct cmdline *l = NULL;
@@ -50,7 +50,7 @@ int main(int argc, char**argpid, char **enpidp) {
 				printf("\n");
 				return 0;
 
-			/* Epidery other command */
+			/* Every other command */
 			} else {
 				/* Debug purpose : to delete */
 				printf("seq[%lu]: %s\n", i, cmd[0]);
@@ -79,15 +79,17 @@ int main(int argc, char**argpid, char **enpidp) {
 					pipe(fd);
 				}
 				
+				/* Command execution handling */
 				int pid = Fork();
-				/* Son execute command with Exec */
+
 				if(pid == 0) {
+					/* Son execute command with Exec */
 
 					if(i == 0) { //If first command
 
 						if(l->in) dup2(open(l->in, 0, O_RDONLY), 0); //has input redirection
 
-					} else { //Following commands get input from pipe
+					} else { //Else following commands get input from pipe
 
 						dup2(pipe_in, 0);
 					}
@@ -97,30 +99,39 @@ int main(int argc, char**argpid, char **enpidp) {
 						if(l->out)
 							dup2(open(l->out, O_WRONLY | O_CREAT , S_IRUSR), 1); //has output redirection
 
-					} else { //
+					} else { //Else previous commands output on stdout
 
 						dup2(fd[1], 1);
-						close(fd[0]);
+
+						close(fd[0]); //Close pipe output 
 
 					}
 
-					execvp(cmd[0], cmd); //ne marche pas si la commande a son PATH complet
+					execvp(cmd[0], cmd); //Si la commande est executée avec un PATH absolu : son argv[0] vaudra le PATH absolu
+
+					/* Debug purpose : to delete */
 					perror("exec failed");
-					return 0;
+
+					return errno;
+
 				} else {
+					/* Father (shell) wait forhis son */
+
 					if(l->seq[i+1]!=NULL)
-						close(fd[1]);
+						close(fd[1]); //Close pipe input
+
 					pipe_in = fd[0];
 
 					int status;
-					while(waitpid(pid, &status, 0),1)
+
+					/* Wait son */
+					while(waitpid(pid, &status, 0),1)q
 						if(WIFEXITED(status))break;
-					printf("exit code %i\n", WEXITSTATUS(status));
 				}
 				
-
+				
 				if(comment_str  != NULL) {
-					cmd[comment_index] = comment_str ;
+					cmd[comment_index] = comment_str ;	
 					break;
 				}
 			}
