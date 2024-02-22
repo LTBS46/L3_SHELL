77caa2c5c63fdd617ae6de93b98363af18783560
Baptiste Saez <baptiste.saez@etu.univ-grenoble-alpes.fr>
2024-02-22 08:49:41 +0100

    run_exec

--- src/shell.c
+++ src/shell.c
@@ -8,18 +8,74 @@
 #include "csapp.h"
 #include <stdbool.h>
 
+int run_cmd(char**cmd, int pipe_in) {
+	/* Pipe handling */
+	int fd[2];
+	if(l->seq[i+1]!=NULL) {
+		pipe(fd);
+	}
+				
+	/* Command execution handling */
+	int pid = Fork();
+
+	if(pid == 0) {
+		/* Son execute command with Exec */
+
+		if(i == 0) { //If first command
+
+			if(l->in) dup2(open(l->in, 0, O_RDONLY), 0); //has input redirection
+
+		} else { //Else following commands get input from pipe
+
+			dup2(pipe_in, 0);
+
+		}
+
+		if(l->seq[i+1] == NULL) { //If last command
+
+			if(l->out)
+				dup2(open(l->out, O_WRONLY | O_CREAT , S_IRUSR), 1); //has output redirection
+
+		} else { //Else previous commands output on stdout
+
+			dup2(fd[1], 1);
+
+			close(fd[0]); //Close pipe output 
+
+		}
+
+		execvp(cmd[0], cmd); //Si la commande est executée avec un PATH absolu : son argv[0] vaudra le PATH absolu
+
+		perror("exec failed");
+
+		exit(errno);
+	} 
+
+	/* Father (shell) wait for his son */
+
+	if(l->seq[i+1]!=NULL)
+		close(fd[1]); //Close pipe input
+
+	int status;
+
+	/* Wait son */
+	if(!(l->is_background)){
+		while(waitpid(pid, &status, 0),1) if(WIFEXITED(status))break;
+	}
+
+	/* Zombi children collect */
+	waitpid(-1, NULL, WNOHANG | WUNTRACED);
+	return fd[0];
+}
+
 
 int main(int argc, char**argv, char **envp) {
-
-
-
 	while (1) {
 		int pipe_in = 0;
 		struct cmdline *l = NULL;
-		printf("shell> ");
+		printf("\033[94mshell> \033[0m");
+		fflush(stdout);
 		l = readcmd();
-
-		
 
 		if (!l) {
 			/* If input stream closed, normal termination */
@@ -41,72 +97,13 @@ int main(int argc, char**argv, char **envp) {
 
 			/* Quit command */
 			if(!strcmp("quit", cmd[0])) {
-
+				if(!isatty(0)) printf("%s", cmd[0]);
 				printf("\n");
 				return 0;
 
 			/* Every other command */
 			} else {
-
-				/* Pipe handling */
-				int fd[2];
-
-				if(l->seq[i+1]!=NULL) {
-					pipe(fd);
-				}
-				
-				/* Command execution handling */
-				int pid = Fork();
-
-				if(pid == 0) {
-					/* Son execute command with Exec */
-
-					if(i == 0) { //If first command
-
-						if(l->in) dup2(open(l->in, 0, O_RDONLY), 0); //has input redirection
-
-					} else { //Else following commands get input from pipe
-
-						dup2(pipe_in, 0);
-					}
-
-					if(l->seq[i+1] == NULL) { //If last command
-
-						if(l->out)
-							dup2(open(l->out, O_WRONLY | O_CREAT , S_IRUSR), 1); //has output redirection
-
-					} else { //Else previous commands output on stdout
-
-						dup2(fd[1], 1);
-
-						close(fd[0]); //Close pipe output 
-
-					}
-
-					execvp(cmd[0], cmd); //Si la commande est executée avec un PATH absolu : son argv[0] vaudra le PATH absolu
-
-					perror("exec failed");
-
-					return errno;
-
-				} else {
-					/* Father (shell) wait for his son */
-
-					if(l->seq[i+1]!=NULL)
-						close(fd[1]); //Close pipe input
-
-					pipe_in = fd[0];
-
-					int status;
-
-					/* Wait son */
-					if(!(l->is_background)){
-						 while(waitpid(pid, &status, 0),1) if(WIFEXITED(status))break;
-					}
-
-					/* Zombi children collect */
-					waitpid(-1, NULL, WNOHANG | WUNTRACED);
-				}
+				pipe_in = run_cmd(cmd, pipe_in);
 			}
 		}
 	}
